f668c2f6aafec11af1834802021f435c01cb6b5a
yixiao.xu.7e9 <yixiao.xu.7e9@itb.cat>
2026-01-08 09:51:15 +0100

    ejercicio dom

---
+++ DOM/Ejercicio1/hacker.js
@@ -0,0 +1,14 @@
+let titular = document.getElementById("titular")
+titular.textContent = "AAAAAAAAAA"
+console.log(titular)
+
+let autor = document.getElementById("autor")
+autor.textContent = "Yixiao Xu"
+console.log(autor)
+
+let noticia = document.getElementById("cuerpo-noticia")
+noticia.textContent = "Me muero"
+console.log(noticia)
+
+let inputValue = document.getElementById("input-comentario")
+inputValue.value = "No me lo creo"

578b7a3ce5099cbcaff2a1a473f1d1d3c3035106
Yixiao <yixiao.xu.7e9@itb.cat>
2026-02-04 17:24:32 +0100

    Minitienda

--- JS/Practica/PR01_Minitienda.js
+++ JS/Practica/PR01_Minitienda.js
@@ -17,30 +17,75 @@ function muestraProductos(){
     }
 }
 
-function crearArticulo(product){
+function crearArticulo(producto){
     const art = document.createElement("article");
     art.className = "producto";
-    art.setAttribute("id", product.id);
-    const img = creaImagen(product);
 
-    article.appendChild(img);
-    article.appendChild(creaTitulo(producto.nombre));
-    article.appendChild(creaDescripcion(producto.descripcion));
-    article.appendChild(creaPrecio(producto.precioBase));
-    article.appendChild(creaSelectorTallas(producto.tallas));
-    article.appendChild(creaSelectorColores(producto, img));
-    article.appendChild(creaBoton(producto));
+    art.setAttribute("id", producto.id);
+    art.appendChild(creaImagen(producto));
+    art.innerHTML += "<span>" + producto.tags[0] + "</span>";
+    art.appendChild(creaTitulo(producto.nombre));
+    art.innerHTML += creaDescripcion(producto.descripcion);
+    art.innerHTML += creaPrecio(producto.precioBase);
+    art.appendChild(creaSelectorTallas(producto.tallas));
+    art.appendChild(creaSelectorColores(producto));
+    art.appendChild(creaBoton());
 
     return art;
 }
 
 function creaImagen(product) {
-    const img = document.createElement("img");
+    const image = document.createElement("img");
 
     const colorInicial = product.colores[0];
-    img.src = product.imagenes[colorInicial];
-    img.alt = product.nombre;
+    image.src = product.imagenes[colorInicial];
+    image.alt = product.nombre;
 
-    return img;
-    
+    return image;
+}
+
+function creaTitulo(titulo){
+    const titul = document.createElement("h3");
+    titul.textContent = titulo;
+    return titul
+}
+
+function creaDescripcion(descripcion) {
+    return "<p>" + descripcion + "</p>";
+}
+
+function creaPrecio(precio) {
+    return "<p>" + `${precio.toFixed(2)} €` + "</p>";
+}
+
+function creaSelectorTallas(tallas) {
+    const select = document.createElement("select");
+
+    for(let talla of tallas){
+        const option = document.createElement("option");
+        option.value = talla;
+        option.textContent = talla;
+        select.appendChild(option);
+    }
+
+    return select;
+}
+
+function creaSelectorColores(producto) {
+    const select = document.createElement("select");
+
+    producto.colores.forEach(color => {
+        const option = document.createElement("option");
+        option.value = color;
+        option.textContent = color;
+        select.appendChild(option);
+    });
+    return select;
+}
+
+function creaBoton() {
+    const button = document.createElement("button");
+    button.textContent = "Añadir al carrito";
+
+    return button;
 }

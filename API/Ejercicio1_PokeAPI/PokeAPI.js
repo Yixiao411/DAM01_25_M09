6a76132ef305d372396844905bbb65645e6aa799
Yixiao <yixiao.xu.7e9@itb.cat>
2026-02-23 15:43:47 +0100

    AWAWAW

--- API/Ejercicio1_PokeAPI/PokeAPI.js
+++ API/Ejercicio1_PokeAPI/PokeAPI.js
@@ -1,13 +1,14 @@
-let pokeIndex = 0;
-let lastPokeIndex = 4;
+let quant = 4;
+let lastPokeIndex = 0;
 
 async function conectaAPI() {
     try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${lastPokeIndex}&offset=${pokeIndex}://api.chucknorris.io/jokes/random`);
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${quant}&offset=${lastPokeIndex}`);
         if (!res.ok) {
             throw new Error(`Error HTTP ${res.status}`);
         }
         const json = await res.json();
+        lastPokeIndex += 4;
         muestraUrls(json);
     } catch (error) {
         console.error("Error:", error);
@@ -28,6 +29,18 @@ async function conectaPokemon(url) {
 }
 
 function muestraPokemons(pokemon){
+    let pokeDIV = document.getElementById("pokemons");
+
+    let poke = document.createElement("div");
+    poke.className = "pokeInfo";
+    let str = "";
+    str += `<p>Nombre: ${pokemon.name}</p>`
+    str += `<img class="foto" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">`
+    str += `<p>Type: ${pokemon.types[0].type.name}</p>`
+    str += `<p>ID: ${pokemon.id}</p>`
+
+    poke.innerHTML += str;
+    pokeDIV.appendChild(poke);
     console.log(pokemon.forms[0].name);
 }
 
